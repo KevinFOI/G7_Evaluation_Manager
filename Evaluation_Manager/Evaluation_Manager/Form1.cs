88861dc3dff8f16b950436f3c8218bee19ca5ff0
somaa9 <kevin.ali02@hotmail.fr>
2023-04-27 15:02:54 +0200

    Creates a new Windows Forms project.

--- Evaluation_Manager/Evaluation_Manager/Form1.cs
+++ Evaluation_Manager/Evaluation_Manager/Form1.cs
@@ -6,10 +6,5 @@ namespace Evaluation_Manager
         {
             InitializeComponent();
         }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
     }
 }
