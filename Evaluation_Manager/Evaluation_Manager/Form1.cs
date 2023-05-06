322018f317748eb6beebe76e33853849b6ef37ad
somaa9 <kevin.ali02@hotmail.fr>
2023-05-06 16:03:51 +0200

    #5 Choose a file and allow only .txt files

--- Evaluation_Manager/Evaluation_Manager/Form1.cs
+++ Evaluation_Manager/Evaluation_Manager/Form1.cs
@@ -9,17 +9,17 @@ namespace Evaluation_Manager
 
         private void button1_Click(object sender, EventArgs e)
         {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "txt files (*.txt)| *.txt";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                UserFileReader filereader = new UserFileReader(ofd.FileName);
+                UserFileParser parser = new UserFileParser();
+                UserFormInfo forminfo = new UserFormInfo(parser.stringParser(filereader.Readfile()));
+                UserDisplayer displayer = new UserDisplayer();
+                displayer.DisplayInfo(this, forminfo);
 
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
+            }
         }
     }
 }
