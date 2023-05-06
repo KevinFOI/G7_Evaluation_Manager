78ee42f2f0170f0f29781feb9c3fd710933130d8
somaa9 <kevin.ali02@hotmail.fr>
2023-05-06 16:07:48 +0200

    #4 Displays user information

---
+++ Evaluation_Manager/Evaluation_Manager/UserDisplayer.cs
@@ -0,0 +1,30 @@
+﻿using Evaluation_Manager;
+using Microsoft.VisualBasic.ApplicationServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluation_Manager
+{
+    internal class UserDisplayer
+    {
+        public void DisplayInfo(Form1 form, UserFormInfo current_UserForm)
+        {
+            if (current_UserForm != null && form != null)
+            {
+                form.Controls["NameTextBox"].Text = current_UserForm.Name;
+                form.Controls["YearOfBirthTextBox"].Text = current_UserForm.YearofBirth;
+                form.Controls["CityTextBox"].Text = current_UserForm.City;
+                form.Controls["FacultyTextBox"].Text = current_UserForm.Faculty;
+                form.Controls["RoleTextBox"].Text = current_UserForm.Role;
+                form.Controls["SpecificAttributeTextBox"].Text = current_UserForm.SpecificAttribute;
+            }
+            else
+            {
+                Console.WriteLine("There is nothing to display");
+            }
+        }
+    }
+}
