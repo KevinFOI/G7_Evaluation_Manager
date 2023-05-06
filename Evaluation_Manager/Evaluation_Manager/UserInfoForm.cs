96ab86ba4f82609fc466217783a43c3a72dc179b
somaa9 <kevin.ali02@hotmail.fr>
2023-05-06 16:21:49 +0200

    #4 Allows for the correct display of information

---
+++ Evaluation_Manager/Evaluation_Manager/UserInfoForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluation_Manager
+{
+    internal class UserInfoForm
+    {
+        public string Name;
+        public string Surname;
+        public string YearofBirth;
+        public string City;
+        public string Faculty;
+        public string Role;
+        public string SpecificAttribute;
+
+        public UserInfoForm(string[] param)
+        {
+            Name = param[0] + " " + param[1];
+            YearofBirth = param[2];
+            City = param[3];
+            Faculty = param[4];
+            Role = param[5];
+            SpecificAttribute = param[6];
+
+        }
+    }
+}
