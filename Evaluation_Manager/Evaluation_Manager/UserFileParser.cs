b729b83ff4241be411aebeb69c266711f27530f0
somaa9 <kevin.ali02@hotmail.fr>
2023-05-06 16:09:57 +0200

    #3 Select information in the .txt file

--- Evaluation_Manager/Evaluation_Manager/UserFileParser.cs
+++ Evaluation_Manager/Evaluation_Manager/UserFileParser.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Evaluation_Manager
 {
     internal class UserFileParser
     {
+        public string[] stringParser(string text)
+        {
+            List<string> list = new List<string>();
+            string[] lines = text.Split('\n');
+
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split('=');
+                if (parts.Length == 2)
+                {
+                    string value = parts[1].Trim();
+                    list.Add(value);
+
+                }
+
+            }
+            return list.ToArray();
+        }
     }
 }
